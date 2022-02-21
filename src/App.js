ed9f2b992ecd66ab59cc009147ac85a6d0b921a6
unknown <andreopbmo@gmail.com>
2022-02-21 21:24:45 +0000

    Add Items component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BiCart } from "react-icons/bi";
+import Items from "./components/Items";
 import Search from "./components/Search";
-
-
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-          <BiCart className="inline-block align-top text-blue-500"/> Your cart
+      <h1 className="text-5xl mb-3">
+          <BiCart className="inline-block align-top text-blue-500"/> Your items
       </h1>
+      <Items />
       <Search />
     </div>
     
