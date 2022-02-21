f51792a2f8c4b1230f4f99fa32a6490eacd65bf3
unknown <andreopbmo@gmail.com>
2022-02-21 01:14:41 +0000

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BiCart } from "react-icons/bi";
+import Search from "./components/Search";
 
-export default function App() {
+
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+          <BiCart className="inline-block align-top text-blue-500"/> Your cart
+      </h1>
+      <Search />
+    </div>
+    
   )
 }
+
+export default App;
